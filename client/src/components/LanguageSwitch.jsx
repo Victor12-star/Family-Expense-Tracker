e01df337baeb7d018a7f29767acc80630ac7f394
Victor12-star <okonv09@gmail.com>
2026-08-29 18:24:47 +0200

    Remove language selector icon

--- client/src/components/LanguageSwitch.jsx
+++ client/src/components/LanguageSwitch.jsx
@@ -1,11 +1,9 @@
-import { Languages } from "lucide-react";
 import { LANGUAGES, useLanguage } from "../context/LanguageContext.jsx";
 
 export default function LanguageSwitch() {
   const { language, setLanguage } = useLanguage();
   return (
     <label className="language-switch" aria-label="Language">
-      <Languages size={17} aria-hidden="true" />
       <select value={language} onChange={(event) => setLanguage(event.target.value)}>
         {LANGUAGES.map(([code, label]) => <option key={code} value={code}>{label}</option>)}
       </select>
